5ea05d8e370f3875e37a9d9565196b6e76d0b8dc
asorokin <a40in@inbox.ru>
2022-05-31 12:28:44 +0300

    _

--- notificator.go
+++ notificator.go
@@ -2,6 +2,7 @@ package notification
 
 import "io"
 
+//go:generate mockgen -destination=mock/notificator_mock.go redits.oculeus.com/asorokin/notification Notificator
 type Notificator interface {
 	SendMessage(message Message, attachments ...Attachment) error
 }
